e2bac14946decde9980c2ff27820936cb957124b
Zaquiel Grings <zaquiel@gmail.com>
2019-05-28 21:05:36 -0300

    Domain and Service
    Implementation of service and domain

---
+++ SmallRedis.Core/Services/ConsoleService.cs
@@ -0,0 +1,36 @@
+﻿using SmallRedis.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmallRedis.Core.Services
+{
+    public class ConsoleService
+    {
+        private RedisinhoBLL _redisinhoBLL;
+
+        public ConsoleService()
+        {
+            _redisinhoBLL = new RedisinhoBLL();
+        }
+
+        public string CommandExecute(string command)
+        {
+            object result = null;
+
+            if (!string.IsNullOrEmpty(command))
+            {
+                var (response, commandFormatted, error) = _redisinhoBLL.FormatCommand(command);
+
+                result = response ? _redisinhoBLL.ExecuteCommand(commandFormatted) : error;
+            }
+
+            return result?.ToString();
+        }
+
+        public void ClearDatabase()
+        {
+            _redisinhoBLL.ClearDatabase();
+        }
+    }
+}
